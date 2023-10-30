2dfec22ca255dcb5dd8ba54cb473fc8b04a300cb
kstrzelecki <strzelecki@nonstop.co>
2023-10-30 19:04:16 +0100

    fixes

--- Test.java
+++ Test.java
@@ -3,5 +3,6 @@ public class Test {
     System.out.println("Hello GIT!!");
     System.out.println("Hello GIT v2!!");
     System.out.println("BRANCH TASK1 !!");
+    System.out.println("FIX!!");
   }
 }
