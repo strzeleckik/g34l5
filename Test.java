b9f78d69c6aba842cb4877bd5cd0b6afd6a6e83f
kstrzelecki <strzelecki@nonstop.co>
2023-10-30 18:59:11 +0100

    changes task 1

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
   public static void main(String[] args) {
     System.out.println("Hello GIT!!");
     System.out.println("Hello GIT v2!!");
+    System.out.println("BRANCH TASK1 !!");
   }
 }
